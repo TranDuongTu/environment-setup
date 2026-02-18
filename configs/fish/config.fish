56e98ceb8069b9378e081cca2e0c86cafca707a3
Tu Tran <ttran_axoncorp@axon.com>
2026-02-18 22:45:41 +0700

    update install.sh for macos

--- configs/fish/config.fish
+++ configs/fish/config.fish
@@ -1,4 +1,9 @@
+if test -d /opt/homebrew/bin
+    fish_add_path /opt/homebrew/bin
+end
+
 if status is-interactive
 # Commands to run in interactive sessions can go here
 end
-export PATH="$HOME/.local/bin:$PATH"
+
+fish_add_path $HOME/.local/bin
